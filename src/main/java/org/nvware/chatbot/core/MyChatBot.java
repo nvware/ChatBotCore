0e9b7018141ccffdc370d8947f11432ae342297c
nvware <h_valizadgan@yahoo.com>
2023-05-20 11:06:27 +0300

    Create

---
+++ src/main/java/org/nvware/chatbot/core/MyChatBot.java
@@ -0,0 +1,38 @@
+package org.nvware.chatbot.core;
+
+//import com.github.pengrad.openshift.ChatBot;
+//import com.github.pengrad.openshift.ChatterBotFactory;
+//import com.github.pengrad.openshift.ChatterBotSession;
+
+public class MyChatBot {
+    public static void main(String[] args) {
+        // Create a new chatbot instance
+//        ChatterBotFactory factory = new ChatterBotFactory();
+//        ChatterBot chatbot;
+//        try {
+//            chatbot = factory.create(ChatterBotType.CLEVERBOT);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return;
+//        }
+//
+//        // Start a new chat session
+//        ChatterBotSession chatSession = chatbot.createSession();
+//
+//        // Chat with the chatbot
+//        String userInput = "Hello, how are you?";
+//        while (true) {
+//            String response = chatSession.think(userInput);
+//            System.out.println("ChatBot: " + response);
+//            // Get user input from console or any other source
+//            userInput = getUserInput();
+//        }
+    }
+
+    private static String getUserInput() {
+        // Implement your logic to get user input
+        // For example, you can use Scanner to read input from the console
+        // or obtain input from any other source
+        return "";
+    }
+}
